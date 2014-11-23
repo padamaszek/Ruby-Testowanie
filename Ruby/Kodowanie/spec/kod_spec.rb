e82482d301b0543e8c212908b0d4fe114069efa5
padamaszek <Ursus707@gmail.com>
2014-11-23 19:33:10 +0100

    RSA and 3 tests

--- Ruby/Kodowanie/spec/kod_spec.rb
+++ Ruby/Kodowanie/spec/kod_spec.rb
@@ -15,4 +15,25 @@ describe "euler" do
     expect(@klasa.euler(p,q)).to eq(120)
   end
 end
+
+describe "modul" do
+  let(:p){p = 13}
+  let(:q){p = 11}
+  it "should return 143" do
+    expect(@klasa.modul(p,q)).to eq(143)
+  end
+end
+
+describe "NWD" do
+  let(:a){a = }
+  let(:b){b = }
+  it "should return xxx" do
+    expect(@klasa.NWD(a,b)).to eq(xxx)
+  end
+end
+
+
+
+
+
 end
